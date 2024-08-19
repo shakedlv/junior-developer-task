1013aa9fdb6585cc4b6712d6e0c561eeb85dcf66
Shaked Levy <shakedlv0@gmail.com>
2024-08-19 20:43:51 +0300

    change endpoints

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(limiter)
 
 app.use('/', metadataRoutes);
-app.get("/api", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(5000, () => {
 	console.log('Server is running on port 5000');
