48ede636dc893ca943fa835b32fadd4748bb73a2
Shaked Levy <shakedlv0@gmail.com>
2024-08-19 20:01:07 +0300

    cors

--- frontend/src/components/input/urlValidation.js
+++ frontend/src/components/input/urlValidation.js
@@ -1,6 +1,5 @@
 function isValidHttpUrl(string){
     const urlPattern = /^(https?:\/\/)(www\.)[a-zA-Z0-9-]+\.[a-z]{2,}([\/?].+)?$/;
-    console.log(urlPattern.test(string))
     return urlPattern.test(string);
 }
 
